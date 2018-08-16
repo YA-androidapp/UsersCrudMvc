029d5abec19c8f5183467dfaa91e1d589d87f83c
YA <YA-androidapp@users.noreply.github.com>
2018-08-16 12:47:38 +0900

    init

---
+++ UsersCrudMvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(UsersCrudMvc.Startup))]
+namespace UsersCrudMvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
