b28d7af2f2fa5e0c6da5f049006c2d0938ea1a79
YA <YA-androidapp@users.noreply.github.com>
2018-08-26 14:46:33 +0900

    テーブル生成
    NuGetパッケージマネージャーコンソールから以下を実行
    
    $ Enable-Migrations
    $ Add-Migration migration_filename
    $ Update-Database

---
+++ UsersCrudMvc/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace UsersCrudMvc.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<UsersCrudMvc.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(UsersCrudMvc.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
